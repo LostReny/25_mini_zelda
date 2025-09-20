f68de85c110d102517380e3e23002863e0af8d06
Isabelle Reny <isabelleribaldo@hotmail.com>
2025-09-20 19:56:58 -0300

    Initial commit

---
+++ zeldaminiclone/src/zeldaminiclone/Game.java
@@ -0,0 +1,42 @@
+package zeldaminiclone;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable{
+
+	public static int WIDTH = 480, HEIGHT = 480;
+	
+	public Game() {
+		this.setPreferredSize(new Dimension(WIDTH,HEIGHT));	
+	}
+	
+	//metodo principal
+	public static void main(String[] args) {
+		Game game = new Game();
+		JFrame frame = new JFrame();
+		
+		frame.add(game);
+		frame.setTitle("The Game");
+		
+		frame.pack();
+		
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
+		
+		new Thread(game).start();
+	}
+	
+	@Override 
+	public void run() {
+		
+		
+	}
+	
+	
+
+}
